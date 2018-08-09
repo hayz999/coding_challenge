c21143c6896960b53df4955bc678b2dd2abf560f
hayz999 <hlzulkoski@gmail.com>
2018-08-09 17:54:52 -0600

    added decimal logic and created an uncommented version

--- app.js
+++ app.js
@@ -9,21 +9,28 @@ const amount = ['', 'thousand', 'million', 'billion', 'trillion']
 function numberToWord(number) {
   //turn number into string so it can later be split
   let numString = number.toString()
+  let wholeNumber = numString[0]
+  let decimal
+  // if there are decimal numbers, slice to just the hundredth place 
+  if (numString.length > 1) {
+    decimal = numString[1].slice(0, 2)
+  }
   //if that number is 0 we want to return right away
-  if (parseInt(numString) === 0) {
+  if (parseInt(wholeNumber) === 0) {
     return 'zero';
   }
   //get the length of the number so it knows how many times to loop
-  let start = numString.length
+  let start = wholeNumber.length
   //set empty array to store 3 number long sections to loop over
   let sections = []
   // while loop to go through the full number and break into 3 integer long sections to push into the sections array
   while(start > 0) {
     let end = start
-    sections.push(numString.slice((start = Math.max(0, start -3)), end))
+    sections.push(wholeNumber.slice((start = Math.max(0, start -3)), end))
   }
   // get the length of the now sectioned number; if it is less than the length of the amount array it means it is less than a thousand, so return
   let sectionsLength = sections.length
+  // if over a trillion, return an empty string
   if(sectionsLength > amount.length) {
     return ''
   }
@@ -35,8 +42,8 @@ function numberToWord(number) {
     let section = parseInt(sections[i])
     if(section) {
       // split each section into individual numbers array
+      // example: number is 213, reverse it to 312. At index 1 is number 1 , we add 10 to make that 13 so we can match to the correct index in the oneToNineteen array
       let numbers = sections[i].split('').reverse().map(parseFloat)
-      // 
       if(numbers[1] === 1) {
         numbers[0] += 10
       }
@@ -58,10 +65,16 @@ function numberToWord(number) {
           numWords.push('and')
         }
       }
+      // if there is a third number in that section add hundred to the end 
       if((word = oneToNineteen[numbers[2]])) {
         numWords.push(word + ' hundred')
       }
     }
   }
+  // add the decimal to the end if it exists  
+  if (decimal) {
+    numWords.unshift('and ' + decimal + '/' + '100')
+  }
+  // return the whole array of words reversed back to the correct order and joined together into one string
   return numWords.reverse().join(' ')
 }
--- index.js
+++ index.js
@@ -4,31 +4,27 @@ const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 
 
 const amount = ['', 'thousand', 'million', 'billion', 'trillion']
 
-let sections = []
-let numWords = []
-
-function numberToString(number) {
+function numberToWord(number) {
   let numString = number.toString()
-  if (parseInt(numString) === 0) {
+  let wholeNumber = numString[0]
+  let decimal
+  if (numString.length > 1) {
+    decimal = numString[1].slice(0, 2)
+  }
+  if (parseInt(wholeNumber) === 0) {
     return 'zero';
   }
-  let start = numString.length
-  breakToSections(start, numString)
-}
-
-function breakToSections(start, numString) {
-  while(start > 0) {
+  let start = wholeNumber.length
+  let sections = []
+  while (start > 0) {
     let end = start
-    sections.push(numString.slice((start = Math.max (0, start - 3)), end))
+    sections.push(wholeNumber.slice((start = Math.max(0, start - 3)), end))
   }
-  turnToWords()
-}
-
-function turnToWords() {
   let sectionsLength = sections.length
-  if(sectionsLength > amount.length) {
+  if (sectionsLength > amount.length) {
     return ''
   }
+  let numWords = []
   for (i = 0; i < sectionsLength; i++) {
     let section = parseInt(sections[i])
     if (section) {
@@ -46,7 +42,7 @@ function turnToWords() {
         numWords.push(word)
       }
       if (numbers[0] || numbers[1]) {
-        if (numbers[2] || !i && sectionsLength) {
+        if (numbers[2] || !i && sectionsLength >= 3) {
           numWords.push('and')
         }
       }
@@ -54,6 +50,9 @@ function turnToWords() {
         numWords.push(word + ' hundred')
       }
     }
+  }
+  if (decimal) {
+    numWords.unshift('and ' + decimal + '/' + '100')
   }
   return numWords.reverse().join(' ')
 }
